e084344b114e471e0882dad77a6a89527d6185f4
spawar92 <s.pawar2@shell.com>
2025-03-03 21:51:08 -0600

    hello world

--- day001/hello.sh
+++ day001/hello.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-
-echo "Compiling and running hello.cu"
+echo "Removing old hello"
 rm -f hello
 
+echo "Compiling and running hello.cu"
 nvcc -arch sm_90 hello.cu -o hello
 
+echo "Running hello"
 ./hello
